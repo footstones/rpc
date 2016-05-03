95b77c0dceb8736b60cc23fc5e055266a016d42f
Wellming Li <wellming.li@howzhi.com>
2016-05-03 16:19:21 +0800

    refactor

--- examples/config.php
+++ examples/config.php
@@ -3,10 +3,12 @@
 return [
     'host' => '0.0.0.0',
     'port' => 10000,
-    'daemonize' => 1,
-    'pid_path' => '/tmp/plumber.pid',
-    'log_path' => '/tmp/plumber.log',
-    'output_path' => '/tmp/plumber.output.log',
+    'daemonize' => 0,
+    'worker_num' => 4,
+    'server_name' => 'example server',
+    'pid_file' => '/tmp/example_server.pid',
+    'log_file' => '/tmp/example_server.log',
+    'output_file' => '/tmp/example_server.output.log',
     'bootstrap' => __DIR__ . '/bootstrap.php',
     'handler' => 'Footstones\\RPC\\Examples\\ExampleServerHandler'
 ];
--- src/ServerDaemon.php
+++ src/ServerDaemon.php
@@ -25,8 +25,12 @@ class ServerDaemon
             case 'restart':
                 $this->restartServer();
                 break;
+            case 'reload':
+                $this->reloadServer();
+                break;
             case 'status':
                 $this->statusServer();
+                break;
             default:
                 break;
         }
@@ -34,39 +38,89 @@ class ServerDaemon
 
     protected function startServer()
     {
+        if (file_exists($this->config['pid_file'])) {
+            $pid = file_get_contents($this->config['pid_file']);
+
+            if (posix_kill($pid, 0)) {
+                exit("Server is already running.\n");
+            }
+        }
+
         $this->server = $this->createHttpServer();
         $this->server->start();
     }
 
     protected function stopServer()
     {
-
+        $pid = file_get_contents($this->config['pid_file']);
+        posix_kill($pid, SIGTERM);
     }
 
     protected function restartServer()
     {
+        $this->stopServer();
+        $this->startServer();
+    }
 
+    protected function reloadServer()
+    {
+        $pid = file_get_contents($this->config['pid_file']);
+        posix_kill($pid, SIGUSR1);
     }
 
     protected function statusServer()
     {
-        
+
     }
 
     private function createHttpServer()
     {
         $server = new swoole_http_server($this->config['host'], $this->config['port'], SWOOLE_BASE);
         $server->set([
-            'http_parse_post' => false
+            'http_parse_post' => false,
+            'daemonize' => empty($this->config['daemonize']) ? 0 : 1,
+            'worker_num' => empty($this->config['worker_num']) ? 4 : $this->config['worker_num'],
         ]);
 
         $class = $this->config['handler'];
 
         $handler = new $class();
 
+        $server->on('Start', array($this, 'onMasterStart'));
+        $server->on('Shutdown', array($this, 'onMasterStop'));
+        $server->on('ManagerStart', array($this, 'onManagerStart'));
+        $server->on('WorkerStart', array($this, 'onWorkerStart'));
+
         $server->on('request', [$handler, 'onRequest']);
 
         return $server;
     }
+
+    public function onMasterStart($server)
+    {
+        swoole_set_process_name(sprintf('%s: master (%s:%s,%s)', $this->getServerName(), $this->config['host'], $this->config['port'], $this->config['config_file']));
+        file_put_contents($this->config['pid_file'], $server->master_pid);
+    }
+
+    public function onMasterStop($serv)
+    {
+        unlink($this->config['pid_file']);
+    }
+
+    public function onManagerStart($server)
+    {
+        swoole_set_process_name(sprintf('%s: manager', $this->getServerName()));
+    }
+
+    public function onWorkerStart($server)
+    {
+        swoole_set_process_name(sprintf('%s: worker (under master #%s)', $this->getServerName(), $server->master_pid));
+    }
+
+    protected function getServerName()
+    {
+        return !empty($this->config['server_name']) ? $this->config['server_name'] : 'footstone server';
+    }
+
 }
 
